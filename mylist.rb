9d8becbb4ef828a474c2966501fed1bbc1e47d61
Sanja <sanja1mandic@gmail.com>
2022-08-18 17:53:55 +0200

    fix linters

--- mylist.rb
+++ mylist.rb
@@ -7,8 +7,10 @@ class MyList
     @list = list
   end
 
-  def each
-    @list.each {|a| yield a}
+  def each(&filter_block)
+    return 'No block given' unless block_given?
+
+    @list.filter(&filter_block)
   end
 end
 
@@ -16,10 +18,10 @@ list = MyList.new(1, 2, 3, 4)
 
 p list.all? { |e| e < 5 }
 
-p list.all? {|e| e > 5}
+p list.all? { |e| e > 5 }
 
-p list.any? {|e| e == 2}
+p list.any? { |e| e == 2 }
 
-p list.any? {|e| e == 5}
+p list.any? { |e| e == 5 }
 
-p list.filter {|e| e.even?}
+p list.filter { |e| e.even? }
