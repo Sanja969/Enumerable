c3b49f45a1bf60b5caae011121d107529cf818de
Sanja <sanja1mandic@gmail.com>
2022-08-18 18:15:09 +0200

    fix linters

--- mylist.rb
+++ mylist.rb
@@ -16,12 +16,12 @@ end
 
 list = MyList.new(1, 2, 3, 4)
 
-p list.all? { |e| e < 5 }
+p(list.all? { |e| e < 5 })
 
-p list.all? { |e| e > 5 }
+p(list.all? { |e| e > 5 })
 
-p list.any? { |e| e == 2 }
+p((list.any? { |e| e == 2 }))
 
-p list.any? { |e| e == 5 }
+p(list.any? { |e| e == 5 })
 
-p list.filter { |e| e.even? }
+p(list.filter(&:even?))
